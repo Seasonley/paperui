19720c63fe754c1e3b9df83401237ceccfadb28f
Seasonley <seasonley@qq.com>
2019-05-27 17:46:09 +0800

    init

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <Button type="primary">Primary</Button>
+    <Button>Default</Button>
+    <Button type="dashed">Dashed</Button>
+    <Button type="danger">Danger</Button>
+  </div>
+</template>
+
+<script>
+import Button from "./components/Button.vue";
+
+export default {
+  name: "app",
+  components: {
+    Button
+  }
+};
+</script>
+<style>
+body {
+  --pri: #000;
+  --lpri: #999;
+  --sec: #ccc;
+  --lsec: #eee;
+  --lt: #fff;
+  font-size: 14px;
+}
+</style>
---
+++ src/components/Button.vue
@@ -0,0 +1,33 @@
+<template>
+  <button>
+    <slot></slot>
+  </button>
+</template>
+
+<script>
+export default {
+  name: "Button",
+  props: {
+    msg: String
+  }
+};
+</script>
+
+<style>
+button {
+  display: inline-block;
+  margin: 0;
+  padding: 0;
+  color: var(--pri);
+  background-color: var(--lt);
+  border:0;
+}
+button[type="primary"] {
+  color: var(--lt);
+  background-color: var(--pri);
+}
+button[type=""] {
+  color: var(--lt);
+  background-color: var(--pri);
+}
+</style>
